309e24b8cefacd6cc12796464fc1f1703429bb2d
JOSEPH-KIBIRU <josephkibiru19@gmail.com>
2026-01-24 14:50:49 +0300

    Create SidebarContext for managing sidebar state

---
+++ src/context/SidebarContext.js
@@ -0,0 +1,14 @@
+// src/context/SidebarContext.js
+import { createContext, useState } from 'react';
+
+export const SidebarContext = createContext();
+
+export const SidebarProvider = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <SidebarContext.Provider value={{ sidebarOpen, setSidebarOpen }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
